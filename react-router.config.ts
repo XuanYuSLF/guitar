68c69fb686beac867888de0e292533df4349daef
XuanYuSLF <77571440+XuanYuSLF@users.noreply.github.com>
2025-12-16 17:50:06 +0800

    Change basename to root path for all environments

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
   // ⚠️ 必须和 vite.config.ts 里的 REPO_NAME 一致
-  basename: process.env.NODE_ENV === "production" ? "/guitar/" : "/",
+  basename: "/",
 } satisfies Config;
